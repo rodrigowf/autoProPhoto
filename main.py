2f8ae1ae4014a2f4aa44e0c86ff4a64516d9b79b
Rodrigo <rodrigowfranco@gmail.com>
2020-02-16 17:56:08 -0300

    adicionando pequenos detalhs q parecem necessarios pra execução

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
+# [START gae_python37_app]
 import flask
 import requests
 from flask_cors import CORS, cross_origin
@@ -274,8 +274,9 @@ if __name__ == '__main__':
     # When running locally, disable OAuthlib's HTTPs verification.
     # ACTION ITEM for developers:
     #     When running in production *do not* leave this option enabled.
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    # os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
     # Specify a hostname and port that are set as a valid redirect URI
     # for your API project in the Google API Console.
-    app.run('localhost', 8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
