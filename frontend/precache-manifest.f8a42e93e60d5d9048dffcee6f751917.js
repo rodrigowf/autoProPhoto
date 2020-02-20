1ab6da97d91bcf17f933c9cbe13f130d82c4c7d0
Rodrigo <rodrigowfranco@gmail.com>
2020-02-20 03:01:24 -0300

    ops, agora q o frontend foi incluido! (o ultimo commit foi alarme falso.. hehe)

---
+++ frontend/precache-manifest.f8a42e93e60d5d9048dffcee6f751917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702346ba7ef09194259f760aca8624bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a7597280f94878e590b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "74ddd7face88381b19a3",
+    "url": "/static/js/2.caae3042.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.caae3042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7597280f94878e590b",
+    "url": "/static/js/main.143c6e58.chunk.js"
+  },
+  {
+    "revision": "925b4d070e0cd3019d0b",
+    "url": "/static/js/runtime-main.65e413b0.js"
+  }
+]);
