19afa5954938ac5aa04fae2ae33a4f23a84c679c
Rodrigo <rodrigowfranco@gmail.com>
2020-02-20 04:33:46 -0300

    subiondo agora o q era pra ser o ultimo commit, mas direito

---
+++ frontend/precache-manifest.a2c593b7a946ec2a81d5ae31db33c23a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af29dc28ba98818deb2b0d547eb76ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1ab330b6607c5f74f30",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "74ddd7face88381b19a3",
+    "url": "/static/js/2.caae3042.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.caae3042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ab330b6607c5f74f30",
+    "url": "/static/js/main.dd3bcaad.chunk.js"
+  },
+  {
+    "revision": "925b4d070e0cd3019d0b",
+    "url": "/static/js/runtime-main.65e413b0.js"
+  }
+]);
