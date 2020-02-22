640524bc6bd3744b3f526eb35681389c1225271e
Rodrigo <rodrigowfranco@gmail.com>
2020-02-22 14:23:27 -0300

    atualizando startup-script

--- startup-script.sh
+++ startup-script.sh
@@ -24,14 +24,45 @@ sudo bash install-logging-agent.sh
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -yq supervisor python3-pip
-pip install --upgrade pip virtualenv
+pip3 install --upgrade pip virtualenv
+
+# Parte CUDA NVidia -------------------------
+
+# Add NVIDIA package repositories
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo apt-get update
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt-get update
+
+# Install NVIDIA driver
+sudo apt-get install --no-install-recommends nvidia-driver-430
+# Reboot. Check that GPUs are visible using the command: nvidia-smi
+
+# Install development and runtime libraries (~4GB)
+sudo apt-get install --no-install-recommends \
+    cuda-10-1 \
+    libcudnn7=7.6.4.38-1+cuda10.1  \
+    libcudnn7-dev=7.6.4.38-1+cuda10.1
+
+
+# Install TensorRT. Requires that libcudnn7 is installed above.
+sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 \
+    libnvinfer-dev=6.0.1-1+cuda10.1 \
+    libnvinfer-plugin6=6.0.1-1+cuda10.1
+
+# END CUDA -----------------------------------
 
 # Account to own server process
 sudo useradd -m -d /home/refotoapp refotoapp
 
 # Fetch source code
 export HOME=/root
-git clone https://github.com/rodrigowf/autoProPhoto.git /opt/app
+sudo git clone https://github.com/rodrigowf/autoProPhoto.git /opt/app
+
+sudo chmod -R 777 /opt/app
 
 # Python environment setup
 virtualenv -p python3 /opt/app/env
@@ -40,6 +71,7 @@ source /opt/app/env/bin/activate
 
 # Set ownership to newly created account
 sudo chown -R refotoapp:refotoapp /opt/app
+sudo chmod -R 754 /opt/app
 
 # Put supervisor configuration in proper place
 sudo cp /opt/app/refoto.conf /etc/supervisor/conf.d/refoto.conf
