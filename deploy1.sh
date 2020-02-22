a8c6ed4d52ba4f9c5e5a13e26365edf285adff6c
Rodrigo <rodrigowfranco@gmail.com>
2020-02-22 14:32:40 -0300

    colocando todas as instalações necessárias (incluindo CUDA) que exige que faça o reboot - por isso separei a instalação em 2 partes

---
+++ deploy1.sh
@@ -0,0 +1,44 @@
+# Copyright 2019 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Echo commands
+set -v
+
+# Install Stackdriver logging agent
+curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+sudo bash install-logging-agent.sh
+
+# Install or update needed software
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -yq supervisor python3-pip
+pip3 install --upgrade pip virtualenv
+
+# Parte CUDA NVidia -------------------------
+
+# Add NVIDIA package repositories
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo apt-get update
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt-get update
+
+# Install NVIDIA driver
+sudo apt-get install --no-install-recommends nvidia-driver-430
+# Reboot. Check that GPUs are visible using the command: nvidia-smi
+
+sudo reboot
+# CONTINUA no próximo arquivo (deploy2.sh) ........
--- deploy2.sh
+++ deploy2.sh
@@ -15,31 +15,8 @@
 # Echo commands
 set -v
 
-# [START refoto_startup_script]
-# Install Stackdriver logging agent
-curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
-sudo bash install-logging-agent.sh
+# ..... CONTINUAÇÃO do arquivo anterior após o REBOOT (deploy1.sh)
 
-# Install or update needed software
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install -yq supervisor python3-pip
-pip3 install --upgrade pip virtualenv
-
-# Parte CUDA NVidia -------------------------
-
-# Add NVIDIA package repositories
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo apt-get update
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt-get update
-
-# Install NVIDIA driver
-sudo apt-get install --no-install-recommends nvidia-driver-430
-# Reboot. Check that GPUs are visible using the command: nvidia-smi
 
 # Install development and runtime libraries (~4GB)
 sudo apt-get install --no-install-recommends \
@@ -54,6 +31,7 @@ sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 \
     libnvinfer-plugin6=6.0.1-1+cuda10.1
 
 # END CUDA -----------------------------------
+
 
 # Account to own server process
 sudo useradd -m -d /home/refotoapp refotoapp
@@ -79,4 +57,3 @@ sudo cp /opt/app/refoto.conf /etc/supervisor/conf.d/refoto.conf
 # Start service via supervisorctl
 sudo supervisorctl reread
 sudo supervisorctl update
-# [END refoto_startup_script]
